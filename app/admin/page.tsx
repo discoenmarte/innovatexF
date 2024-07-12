8aca5eea4f3684b8fc0d899e2621c609c44fd816
discoenmarte <disco.en.marte@gmail.com>
2024-07-12 16:58:38 -0500

    Ask for permissions always and allow full screen

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,22 +1,7 @@
 'use client'
 
-import { RegionCombox } from '@/components/dashboard/RegionCombox'
-import GetStudentsConnections from '@/components/dashboard/GetStudentsConnectedNum'
-import { Button } from '@/components/ui/button'
-import { DatePickerWithRange } from '@/components/ui/date-picker-range'
-import { DashboardFilters } from '@/types/dashboard'
-import { Filter, UsersRound } from 'lucide-react'
-import { useEffect, useState } from 'react'
-import { DateRange } from 'react-day-picker'
-import { ProgramCombox } from '@/components/dashboard/ProgramCombox'
-import HeadquarterCombox from '@/components/dashboard/HeadquarterCombox'
-import { StudentsDataConnections } from '@/types/metrics'
-import GetStudentsConnectedPer from '@/components/dashboard/GetStudentsConnectedPer'
+import { useEffect } from 'react'
 import { useSession } from 'next-auth/react'
-import { number } from '@/lib/utils/formatter'
-import NormalDistributionGamification from '@/components/dashboard/NormalDistributionGamification'
-import MentorQuestionsTable from '@/components/mentor/MentorQuestionsTable'
-import BestBuddyQuestionTable from '@/components/best-buddy/BestBuddyQuestionsTable'
 import CryptoJS from 'crypto-js' // Assumes @types/crypto-js installed
 
 const encryptionKey = CryptoJS.enc.Utf8.parse('1234567890123456'); // Your encryption key
@@ -33,87 +18,46 @@ function encryptSessionData(sessionData: any): string {
 }
 
 export default function Dashboard() {
-    const [program, setProgram] = useState<string | undefined>()
-    const [region, setRegion] = useState<string | undefined>()
-    const [headquarter, setHeadquarter] = useState<string | undefined>()
-    const [date, setDate] = useState<DateRange | undefined>()
-    const [filterValues, setfilterValues] = useState<DashboardFilters>()
-    const [mentorStudentsConnected, setMentorStudentsConnected] =
-        useState<StudentsDataConnections>()
-    const [bestBuddyStudentsConnected, setBestBuddyStudentsConnected] =
-        useState<StudentsDataConnections>()
-
-    const { data: session } = useSession()
+    const { data: session, status } = useSession()
 
     useEffect(() => {
-        fieldsValidation()
-    }, [region, date, program, headquarter])
+        console.log("Session data:", session);
+        console.log("Session status:", status);
+        requestCameraAndMicPermissions()
+    }, [session])
 
-    const fieldsValidation = () =>
-        !region && !date && !program && setfilterValues(undefined)
-
-    /**
-     * Sets the region and date state variables.
-     *
-     * @return {void} This function does not return anything.
-     */
-    const setRegionAndDate = () => {
-        if (!region && !date) {
-            setfilterValues(undefined)
-        } else {
-            setfilterValues({ region, program, headquarter, date })
+    const requestCameraAndMicPermissions = async () => {
+        try {
+            await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+            console.log("Camera and microphone permissions granted");
+        } catch (err) {
+            console.error("Error requesting camera and microphone permissions:", err);
         }
     }
 
     const encryptedSessionData = session ? encryptSessionData(session) : '';
     const iframeSrc = `https://bot-dev.aitopstaff.com/BusinessAssistant/3/?data=${encodeURIComponent(encryptedSessionData)}`;
 
-    console.log('Iframe Source:', iframeSrc); // Debugging line
-
     return (
-        <section className="flex flex-1 flex-col gap-4  md:p-4 lg:p-6 lg:gap-6 " style={{height: '100vh'}}>
+        <section className="flex flex-1 flex-col gap-4 md:p-4 lg:p-6 lg:gap-6" style={{height: '100vh'}}>
             <article className="grid gap-4">
                 <div className="flex items-center justify-between">
                     <div className="grid">
                         <h1 className="title">Tablero</h1>
                     </div>
                 </div>
-
-                <p>Rengi</p>
-
-                {/* <div className="grid md:grid-flow-col gap-3 md:justify-end items-center">
-                    <ProgramCombox setProgram={setProgram} />
-                    <RegionCombox setRegion={setRegion} />
-                    <HeadquarterCombox setHeadquarter={setHeadquarter} />
-                    <DatePickerWithRange setDate={setDate} />
-                    <Button
-                        disabled={!region && !date}
-                        className="flex gap-2"
-                        size="sm"
-                        onClick={() => setRegionAndDate()}
-                    >
-                        <Filter size={16} />
-                        Filtrar
-                    </Button>
-                </div> */}
             </article>
-            {/* <NormalDistributionGamification filterValues={filterValues} />
-            <GetStudentsConnections
-                filterValues={filterValues}
-                setMentorStudentsConnected={setMentorStudentsConnected}
-                setBestBuddyStudentsConnected={setBestBuddyStudentsConnected}
-            />
-            <GetStudentsConnectedPer
-                mentorConnections={mentorStudentsConnected?.connections}
-                bestBuddyConnections={bestBuddyStudentsConnected?.connections}
-                filterValues={filterValues}
-            />
-            <MentorQuestionsTable filterValues={filterValues} />
-            <BestBuddyQuestionTable filterValues={filterValues} /> */}
 
             <div className="flex justify-center items-center h-full">
                 {encryptedSessionData && (
-                    <iframe src={iframeSrc} width="80%" height="600" frameBorder="0"></iframe>
+                    <iframe 
+                        src={iframeSrc} 
+                        width="80%" 
+                        height="600" 
+                        frameBorder="0" 
+                        allow="fullscreen" 
+                        allowFullScreen
+                    ></iframe>
                 )}
             </div>
         </section>
