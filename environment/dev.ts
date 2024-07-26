04c19f299360ff8cb297b6ff8cf99b0f36223c32
SebastianBlandon <blandonodz@gmail.com>
2024-07-26 17:35:52 -0500

    Feat change env files

--- environment/dev.ts
+++ environment/dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
     ENV: 'dev',
     MAIN_DOMAIN: 'localhost:8000',
-    LOGIN_URL: 'http://localhost:3000/',
-    BASE_URL: 'http://54.225.197.41:8000/api/',
+    LOGIN_URL: 'https://innova.aitopstaff.com/',
+    BASE_URL: 'https://innova-server.aitopstaff.com/api/',
 }
