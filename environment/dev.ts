409942ee69bece772a4bd573f9dc84391d6ba923
SebastianBlandon <blandonodz@gmail.com>
2024-08-13 14:05:42 -0500

    Feat change redirect url to innova-dev

--- environment/dev.ts
+++ environment/dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
     ENV: 'dev',
     MAIN_DOMAIN: 'localhost:8000',
-    LOGIN_URL: 'https://innova.aitopstaff.com/',
+    LOGIN_URL: 'https://innova-dev.aitopstaff.com/',
     BASE_URL: 'https://innova-server.aitopstaff.com/api/',
 }
