bd33e4a94e938d8481a82229e894bfcdd4a6864b
SebastianBlandon <blandonodz@gmail.com>
2024-09-04 11:20:10 -0500

    Fix LOGIN_URL variable

--- environment/dev.ts
+++ environment/dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
     ENV: 'dev',
     MAIN_DOMAIN: 'http://localhost:8000',
-    LOGIN_URL: 'http://localhost:3000',
+    LOGIN_URL: 'https://innova-dev.aitopstaff.com/',
     BASE_URL: 'https://innova-server.aitopstaff.com/api/',
 }
