a6b33d42db9d28388e06b8cfeea3b37a5278b46b
SebastianBlandon <blandonodz@gmail.com>
2024-09-04 10:58:00 -0500

    Feat changed for new view

--- environment/dev.ts
+++ environment/dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
     ENV: 'dev',
-    MAIN_DOMAIN: 'localhost:8000',
-    LOGIN_URL: 'localhost:3000',
-    BASE_URL: 'localhost:3000',
+    MAIN_DOMAIN: 'http://localhost:8000',
+    LOGIN_URL: 'http://localhost:3000',
+    BASE_URL: 'https://innova-server.aitopstaff.com/api/',
 }
